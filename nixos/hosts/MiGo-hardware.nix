142c1a4dd4836c95e3e48dc0e56e145c1eba8d7e
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-03-28 14:26:27 +0000

    Update hardware config for ssd migration

--- nixos/hosts/MiGo-hardware.nix
+++ nixos/hosts/MiGo-hardware.nix
@@ -8,22 +8,18 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2f9ca499-1c52-444c-b304-8e8fa6a4dcad";
+    { device = "/dev/disk/by-uuid/f2bef369-3ae9-49e7-abba-82a1aec3a5a2";
       fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1628-6651";
-      fsType = "vfat";
     };
 
   swapDevices = [ ];
 
   nix.maxJobs = lib.mkDefault 4;
-  powerManagement.cpuFreqGovernor = "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
