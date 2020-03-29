65d5701a4843d8f80bc84f2e2e441acdbda84dcf
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-03-29 02:07:39 +0100

    Add boot drive you dummy

--- nixos/modules/mount.nix
+++ nixos/modules/mount.nix
@@ -4,10 +4,16 @@
   environment.systemPackages = with pkgs; [
     exfat
     ntfs3g
+    efibootmgr
   ];
 
   fileSystems."/data" =
   { device = "/dev/disk/by-uuid/3335d971-c264-4658-b012-94425b29852b";
     fsType = "ext4";
+  };
+
+  fileSystems."/boot" =
+  { device = "/dev/disk/by-label/EFI";
+    fsType = "vfat";
   };
 }
