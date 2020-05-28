9a5198150546515a73170adfa158493289d238ee
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-05-28 17:47:59 +0100

    stuff

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -22,3 +22,6 @@ alias nix-reb="sudo nixos-rebuild switch"
 alias nix-up="sudo nixos-rebuild --upgrade switch"
 alias nix-garb="sudo nix-collect-garbage -d"
 alias nix-chan="echo System channels:&&sudo nix-channel --list && echo User channels:&&nix-channel --list"
+
+alias gs="cd ~/dev/aspire-supply-chain"
+alias gh="cd ~"
--- zsh/mac-specific.sh
+++ zsh/mac-specific.sh
@@ -1,2 +1,13 @@
 source $HOME/.nix-profile/etc/profile.d/nix.sh
 eval "$(pyenv init -)"
+export PATH=~/.npm-global/bin:$PATH
+export AZURE_DEFAULT_DURATION_HOURS=12
+alias aal="aws-azure-login --profile avocado-nonprod --no-prompt; eval $(assume-role avocado-nonprod)"
+
+alias ls="ls -G"
+alias l="ls -G"
+alias ll="ls -lG"
+alias la="ls -AG"
+alias lal="ls -AGl"
+
+alias gs="cd ~/dev/jsainsbury/repo-avocado"
