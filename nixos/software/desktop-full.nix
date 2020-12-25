53cadd74ebb95b671187f0b57b57ad2ad77d8ec9
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-25 02:45:10 +0000

    restructure some more apps

--- nixos/software/desktop-full.nix
+++ nixos/software/desktop-full.nix
@@ -3,21 +3,17 @@
 {
   imports =
     [
-      ../../nixos/software/modules/flatpak.nix
-      ../../nixos/software/modules/fonts.nix
-      ../../nixos/software/modules/power-management.nix
       ../../nixos/software/modules/ssh.nix
       ../../nixos/software/modules/themes.nix
       ../../nixos/software/modules/users.nix
-      ../../nixos/software/modules/nix.nix
       ../../nixos/software/modules/personal.nix
 
       ../../nixos/software/modules/desktop/i3.nix
       ../../nixos/software/modules/desktop/apps/blender.nix
       ../../nixos/software/modules/desktop/apps/firefox.nix
       ../../nixos/software/modules/desktop/apps/gimp.nix
+      ../../nixos/software/modules/desktop/apps/flatpak.nix
       ../../nixos/software/modules/desktop/apps/godot.nix
-      ../../nixos/software/modules/desktop/apps/misc.nix
       ../../nixos/software/modules/desktop/apps/spotify.nix
       ../../nixos/software/modules/desktop/apps/thunar.nix
       ../../nixos/software/modules/desktop/apps/thunderbird.nix
@@ -31,6 +27,12 @@
       ../../nixos/software/modules/desktop/games/misc.nix
       ../../nixos/software/modules/desktop/games/steam.nix
       ../../nixos/software/modules/desktop/terms/urxvt.nix
+      ../../nixos/software/modules/desktop/fonts.nix
+      ../../nixos/software/modules/desktop/power-management.nix
+
+      ../../nixos/software/modules/tools/pentest.nix
+      ../../nixos/software/modules/tools/nix.nix
+      ../../nixos/software/modules/tools/misc.nix
 
       ../../nixos/software/modules/dev/docker.nix
       ../../nixos/software/modules/dev/git.nix
