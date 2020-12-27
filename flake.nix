d6f417293eb764a8e6724c85dca84db2300e636e
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-27 18:23:18 +0000

    input home-manager

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url          = "github:NixOS/nixpkgs/nixos-20.09";
     nixpkgs-unstable.url = "nixpkgs/master";
+    home-manager.url     = "github:rycee/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, ... }: {
