328c521b72022afddea2a0a19fee16dc7d083880
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-28 16:46:42 +0000

    firefox = firefox!

--- modules/desktop/apps/firefox.nix
+++ modules/desktop/apps/firefox.nix
@@ -2,22 +2,6 @@
 
 {
   environment.systemPackages = with pkgs; [
-    xfce.thunar
-    xfce.gvfs
-    gvfs
     firefox
-    thunderbird
-    transmission-gtk
-    atom
-    emacs
-    spotify
-    usbutils
-    unar
-    filezilla
-    ncdu
-    gimp
-    godot
-    blender
-    neofetch
   ];
 }
