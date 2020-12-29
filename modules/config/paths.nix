e42c6dc8a8c709e03c8e1c7649deabb8c88208f9
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-29 00:32:14 +0000

    env vars for useful stuff

--- modules/config/paths.nix
+++ modules/config/paths.nix
@@ -1,14 +1,18 @@
 { self, config, ... }:
 
+with config;
 {
-  config = {
-    local = rec {
-      dotfilesDir = toString ../../.;
-      binDir      = "${dotfilesDir}/bin";
-      configDir   = "${dotfilesDir}/config";
-      hostsDir    = "${dotfilesDir}/hosts";
-      modulesDir  = "${dotfilesDir}/modules";
-      profilesDir = "${dotfilesDir}/profiles";
-    };
+  local = rec {
+    dotfilesDir = toString ../../.;
+    binDir      = "${dotfilesDir}/bin";
+    configDir   = "${dotfilesDir}/config";
+    hostsDir    = "${dotfilesDir}/hosts";
+    modulesDir  = "${dotfilesDir}/modules";
+    profilesDir = "${dotfilesDir}/profiles";
+  };
+  environment.variables = with local; {
+    DOTFILES        = dotfilesDir;
+    DOTFILES_BINS   = binDir;
+    DOTFILES_CONFIG = configDir;
   };
 }
