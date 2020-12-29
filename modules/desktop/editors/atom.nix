f1c53e657fad068aebebbd27878fe96b6202b805
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-29 02:49:30 +0000

    atom declarative config (except packages)

--- modules/desktop/editors/atom.nix
+++ modules/desktop/editors/atom.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    atom
-  ];
+  environment.variables = {
+    ATOM_HOME = "$XDG_CONFIG_HOME/atom";
+  };
+  environment.systemPackages = with pkgs; [ atom ];
+  home.configFile = with config.local; {
+    "atom" = { source = "${configDir}/atom"; recursive = true; };
+  };
 }
