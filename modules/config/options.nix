e8ac9be467b667033edc5a62165beea9c849b3f4
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-29 20:22:03 +0000

    env.path stuff

--- modules/config/options.nix
+++ modules/config/options.nix
@@ -11,5 +11,14 @@ with lib; {
       configFile = mkOption { type = attrs; };
       dataFile = mkOption { type = attrs; };
     };
+    env = mkOption {
+      type = attrsOf (oneOf [ str path (listOf (either str path)) ]);
+      apply = mapAttrs
+        (n: v: if isList v
+           then concatMapStringsSep ":" (x: toString x) v
+           else (toString v));
+      default = {};
+      description = "Copied wholesale from @hlissner, cheers.";
+    };
   };
 }
--- modules/config/paths.nix
+++ modules/config/paths.nix
@@ -1,17 +1,24 @@
-{ self, config, ... }:
+{ self, config, lib, ... }:
 
-with config; {
-  local = rec {
-    dotfilesDir = toString ../../.;
-    binDir = "${dotfilesDir}/bin";
-    configDir = "${dotfilesDir}/config";
-    hostsDir = "${dotfilesDir}/hosts";
-    modulesDir = "${dotfilesDir}/modules";
-    profilesDir = "${dotfilesDir}/profiles";
-  };
-  environment.variables = with local; {
-    DOTFILES = dotfilesDir;
-    DOTFILES_BINS = binDir;
-    DOTFILES_CONFIG = configDir;
+{
+  config = {
+    local = rec {
+      dotfilesDir = toString ../../.;
+      binDir      = "${dotfilesDir}/bin";
+      configDir   = "${dotfilesDir}/config";
+      hostsDir    = "${dotfilesDir}/hosts";
+      modulesDir  = "${dotfilesDir}/modules";
+      profilesDir = "${dotfilesDir}/profiles";
+    };
+    environment.variables = {
+      DOTFILES        = config.local.dotfilesDir;
+      DOTFILES_BINS   = config.local.binDir;
+      DOTFILES_CONFIG = config.local.configDir;
+    };
+    env.PATH = [ "$XDG_CONFIG_HOME/dotfiles/bin" "$PATH" ];
+    
+    environment.extraInit =
+      builtins.concatStringsSep "\n"
+        (lib.attrsets.mapAttrsToList (n: v: "export ${n}=\"${v}\"") config.env);
   };
 }
