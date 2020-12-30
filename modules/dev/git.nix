cd7e8568bc2613076add94a09e635672957d6719
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-30 14:22:02 +0000

    gitignore for real

--- modules/dev/git.nix
+++ modules/dev/git.nix
@@ -2,12 +2,6 @@
 
 {
   environment.systemPackages = with pkgs; [ git ];
-  environment.shellAliases = {
-    gst = "git status";
-    gc = "git commit";
-    gd = "git diff";
-    gcm = "git checkout master";
-  };
   home.configFile = with config.local; {
     "git/config".source = "${configDir}/git/config";
   };
