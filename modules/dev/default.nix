9a70933c349bca5a771dfb7cad8b822615c1e068
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-30 23:01:24 +0000

    import blob

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -1,14 +1,9 @@
 { options, config, lib, ... }:
-
-with builtins; with lib;
-let
+with builtins; with lib; let
   path = dir: f: "${toString dir}/${f}";
   isNixSubmodule = f:
     f != "default.nix" &&
       substring 0 1 f != "_" &&
         substring (stringLength f - 4) (stringLength f) f == ".nix";
   submodulesInDir = dir: map (path dir) (filter isNixSubmodule (attrNames (readDir dir)));
-in
-{
-  imports = submodulesInDir ./.;
-}
+in { imports = submodulesInDir ./.; }
