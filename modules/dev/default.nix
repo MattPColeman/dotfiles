017911e064db5c7467478078152daa7d4c8c9f84
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-30 22:22:06 +0000

    fixez

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -1,12 +1,12 @@
 { options, config, lib, ... }:
 
-with builtins; with lib; with types;
+with builtins; with lib;
 let
   isNixSubmodule = f:
     f != "default.nix" &&
     substring 0 1 f != "_" &&
     substring (stringLength f - 4) (stringLength f) f != ".nix";
-  submodulesInDir = dir: filter isNixSubmodule (attrNames (readDir ./.));
+  submodulesInDir = dir: filter isNixSubmodule (attrNames (readDir dir));
 in
 {
   imports = [ submodulesInDir ./. ];
