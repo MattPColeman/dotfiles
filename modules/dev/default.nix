231fc1976148fa94171d5fadbde7bcf287781176
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-30 21:09:48 +0000

    try this on

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -1,7 +1,7 @@
-{ options, config, ... }:
+{ options, config, lib, ... }:
 
 let
-  mapFilterAttrs = pred: f: attrs: filterAttrs pred (mapAttrs' f attrs);
+  mapFilterAttrs = pred: f: attrs: lib.filterAttrs pred (mapAttrs' f attrs);
 in
 let
   modulesInDir = dir:
