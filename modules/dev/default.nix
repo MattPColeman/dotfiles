0517ad10aa55ef84ad72ad7b78b1e985e331bdb5
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-30 22:29:41 +0000

    try this

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -5,9 +5,9 @@ let
   isNixSubmodule = f:
     f != "default.nix" &&
     substring 0 1 f != "_" &&
-    substring (stringLength f - 4) (stringLength f) f != ".nix";
+    substring (stringLength f - 4) (stringLength f) f == ".nix";
   submodulesInDir = dir: filter isNixSubmodule (attrNames (readDir dir));
 in
 {
-  imports = [ submodulesInDir ./. ];
+  imports = submodulesInDir ./.;
 }
