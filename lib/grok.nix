258ebf425c775618322bfdc4a2f99b88f6e4a701
Matt Coleman <matt.coleman@sainsburys.co.uk>
2020-12-31 17:11:51 +0000

    lib unify

--- lib/grok.nix
+++ lib/grok.nix
@@ -3,7 +3,14 @@
 with builtins;
 with lib;
 rec {
-  pathify = dir: filename: "${toString dir}/${filename}";
-  nixfilesInDir = d:
-    map (pathify d) (attrNames (filterAttrs (n: t: t == "regular" && hasSuffix ".nix" n) (readDir d)));
+  isNixFile = filename:
+    hasSuffix ".nix" filename;
+  listFiles = dir:
+    map (x: "${toString dir}/${x}") (attrNames (filterAttrs (x: y: y == "regular") (readDir dir)));
+  listFilesRec = dir:
+    flatten (mapAttrsToList (n: t: if t == "regular" then "${toString dir}/${n}" else listModulesRec (toPath "/${toString dir}/${n}")) (readDir dir));
+  listModules = dir:
+    filter isNixFile (listFiles dir);
+  listModulesRec = dir:
+    filter isNixFile (listFilesRec dir);
 }
--- modules/default.nix
+++ modules/default.nix
@@ -1,11 +1,7 @@
-{ options, config, lib, ... }:
+{ lib, ... }:
 
 with builtins;
 with lib;
-let
-  listModulesRec = dir:
-    filter (f: hasSuffix ".nix" f) (flatten (mapAttrsToList (n: t: if t == "regular" then "${toString dir}/${n}" else listModulesRec (toPath "/${toString dir}/${n}")) (readDir dir)));
-in
 {
   imports = filter (mod: baseNameOf mod != "default.nix") (listModulesRec ./.);
 }
