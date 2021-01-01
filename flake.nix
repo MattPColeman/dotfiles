147da3d78db571687cf27cf9ef0f4efed5b2b4f2
Matt Coleman <matt.coleman@sainsburys.co.uk>
2021-01-01 18:16:38 +0000

    update inputs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    nixpkgs-unstable.url = "nixpkgs/master";
     home-manager = {
       url                    = "github:nix-community/home-manager/release-20.09";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,6 +15,7 @@
       inherit (lib) toLower removeSuffix;
 
       pkgs  = nixpkgs;
+      uPkgs = nixpkgs-unstable;
       lib   = nixpkgs.lib.extend (self: super: { my = import ./lib { lib = self; }; });
 
       extraModules = [ home-manager.nixosModules.home-manager ];
