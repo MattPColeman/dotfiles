e429b0520ff928bc390df97131135833a0b73f74
Matt Coleman <matt.coleman@sainsburys.co.uk>
2021-01-01 18:45:15 +0000

    Can hide modules with _

--- lib/default.nix
+++ lib/default.nix
@@ -5,12 +5,14 @@ with lib;
 rec {
   isNixFile = filename:
     hasSuffix ".nix" filename;
+  isNotHidden = filename:
+    !hasPrefix "_" filename;
   listFiles = dir:
     map (x: "${toString dir}/${x}") (attrNames (filterAttrs (x: y: y == "regular") (readDir dir)));
   listFilesRec = dir:
     flatten (mapAttrsToList (n: t: if t == "regular" then "${toString dir}/${n}" else listModulesRec (toPath "/${toString dir}/${n}")) (readDir dir));
   listModules = dir:
-    filter isNixFile (listFiles dir);
+    filter isNotHidden (filter isNixFile (listFiles dir));
   listModulesRec = dir:
-    filter isNixFile (listFilesRec dir);
+    filter isNotHidden (filter isNixFile (listFilesRec dir));
 }
