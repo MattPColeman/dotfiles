4ae645824d7958969365a534f6548a730fb1c172
Matt Coleman <matt.coleman@sainsburys.co.uk>
2021-01-01 18:36:24 +0000

    label disks, cleaner host conf

--- hosts/MiGo.nix
+++ hosts/MiGo.nix
@@ -1,34 +1,63 @@
 { config, lib, pkgs, ... }:
 
-{
-  # This is impure, which makes the flake sad. I don't know the consequences of ignoring it.
-  # imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
+/*  ../hosts/MiGo.nix
 
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" ];
+    Desktop with:
+      i5 3570k
+      gtx 970
+      3440x1440 monitor
+
+    Assumes:
+      * disk labelled "NIXOS", mounted at /
+      * disk labelled "DATA",  mounted at /data
+      * disk labelled "EFI"",  mounted at /boot
+
+    Specific tweaks for hardware:
+      * Key remapping (ducky mini)
+      * Pulseaudio conf allows HDMI / line-out simultaneously
+*/
+
+{
+  environment.variables.CURRENT_SYSTEM_HOST = "migo";
+  system.stateVersion = "20.09";
+  imports = [ ./interfaces/pulseaudio.nix ./interfaces/ducky.nix ];
+
+  # KERNEL
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  # FILESYSTEM
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/f2bef369-3ae9-49e7-abba-82a1aec3a5a2";
+    device = "/dev/disk/by-label/NIXOS";
     fsType = "ext4";
   };
 
-  swapDevices = [ ];
+  fileSystems."/data" = {
+    device = "/dev/disk/by-label/DATA";
+    fsType = "ext4";
+  };
 
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/EFI";
+    fsType = "vfat";
+  };
+
+  swapDevices = [ ]; # Should probably have some swap...
+
+  # CPU
   nix.maxJobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
-  imports = [
-    ./drivers/nvidia.nix
-    ./drivers/pulseaudio.nix
-    ./interfaces/ducky.nix
-    ./interfaces/efi.nix
-    ./interfaces/volumes.nix
-  ];
-
-  system.stateVersion = "20.09";
-
-  environment.variables.CURRENT_SYSTEM_HOST = "migo";
+  # GPU
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "nvidia" ];
+  };
+  
+  # BOOT
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  environment.systemPackages = with pkgs; [ efibootmgr ];
 }
