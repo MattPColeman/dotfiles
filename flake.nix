f321be0e69ec708a17a537aeaff23aa9d49252ec
Matt Coleman <matt.coleman@sainsburys.co.uk>
2021-01-02 14:48:56 +0000

    indents

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     nixpkgs-unstable.url = "nixpkgs/master";
     home-manager = {
-      url                    = "github:nix-community/home-manager/release-20.09";
+      url = "github:nix-community/home-manager/release-20.09";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
