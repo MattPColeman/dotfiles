90132ae369da2fbdac76097626bfe075b9babcc1
Matt Coleman <matt.coleman@sainsburys.co.uk>
2021-01-02 20:35:10 +0000

    redundant prefix opt

--- test_modules/options.nix
+++ test_modules/options.nix
@@ -3,7 +3,6 @@
 with lib;
 {
   options = with types; {
-    system = mkOption { type = attrs; };
     users.users.${username} = {
       initialHashedPassword = mkAliasDefinitions options.user.${username}.initialHashedPassword;
       uid = mkAliasDefinitions options.user.${username}.uid;
