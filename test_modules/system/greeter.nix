aa16d584c602935afca094a152ea5cd7bfcd9e70
Matt Coleman <matt.coleman@sainsburys.co.uk>
2021-01-03 01:09:48 +0000

    fix

--- test_modules/system/greeter.nix
+++ test_modules/system/greeter.nix
@@ -1,9 +1,9 @@
 { options, config, lib, pkgs, ... }:
 
-with lib; let cfg = config.modules.desktop.greeter.lightdm; in
+with lib; let cfg = config.system.greeter.lightdm; in
 {
   options.system.greeter.lightdm.enable = mkEnableOption "lightdm";
-  
+
   config = mkIf cfg.enable {
     services.xserver.displayManager.lightdm = {
       enable = true;
