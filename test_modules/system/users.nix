8836800d3cd577ae982f272229e3e33ddab91b53
Matt Coleman <matt.coleman@sainsburys.co.uk>
2021-01-03 01:05:53 +0000

    ??

--- test_modules/system/users.nix
+++ test_modules/system/users.nix
@@ -1,20 +1,11 @@
 { config, home-manager, lib, options, ... }:
 
-/* let
-  mkUserOpts = { name, ... }: {
-    options.user = mkAliasDefinitions options.users.users.${name};
-    };
-  };
-in */
 with lib;
 {
 
-  options.user = mkAliasDefinitions options.users.users;
-   /* {
-      default = {};
-      type = with types; attrsOf (submodule mkUserOpts);
-    }; */
-
+  options = {
+    user = mkAliasDefinitions options.users.users;
+  };
   config = {
     security.sudo.wheelNeedsPassword = false;
     users.mutableUsers = false;
