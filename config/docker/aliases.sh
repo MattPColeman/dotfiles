728ec5faf7a93b17d131e0e21089911e15944e12
Matt Coleman <matt.coleman@sainsburys.co.uk>
2021-01-15 12:31:50 +0000

    Consolidate aliases/env vars

--- config/docker/aliases.sh
+++
@@ -1,4 +0,0 @@
-alias dk="docker kill";
-alias dps="docker ps";
-alias dka="echo Killing containers:&& docker ps -q | xargs docker kill";
-alias dwatch="watch docker ps"
--- config/zsh/environment.sh
+++ config/zsh/environment.sh
@@ -1,3 +1,1 @@
 # Common env vars go here
-
-source $ZDOTDIR/extra_environment.sh
